9a5086dc177b0877ce4f0e07f14d88881f3befd7
mina987 <erm.555.mmm@gmail.com>
2024-01-25 11:14:47 +0300

    save file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Save);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(494, 326);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(494, 297);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker3.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 23);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(200, 23);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 186);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(200, 23);
+            this.textBox6.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using Aspose.Words;
+using Aspose.Words.Replacing;
 using ExamsCDSP.Properties;
 using System.Collections;
 using System.Globalization;
@@ -12,6 +13,7 @@ namespace ExamsCDSP
         static CultureInfo ci = CultureInfo.CurrentCulture;
         static DateTime Now = DateTime.Now;
         Document doc;
+
         int DateD = Now.Day;
         string DateM = ci.DateTimeFormat.GetMonthName(Now.Month);
         int DateY = Now.Year;
@@ -54,9 +56,51 @@ namespace ExamsCDSP
             doc = new Document(new MemoryStream(Resources.report));
             InitializeComponent();
         }
-        void FileUpdate()
+        void Save(object sender, EventArgs e)
         {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "DOCX Files (*.docx)|*.docx|All Files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                doc.Range.Replace("DateD", DateD.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("DateM", DateM, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("DateY", DateY.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("Number", Number.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("OrganizationType", OrganizationType, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("OrganizationName", OrganizationName, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WHOM", WHOM, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("OKOPF", OKOPF, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("OKFS", OKFS, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("ORGN", ORGN, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("INN", INN, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("KPP", KPP, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("OKVED", OKVED, new FindReplaceOptions(FindReplaceDirection.Forward));
 
+                doc.Range.Replace("PHONE", PHONE, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("EMAIL", EMAIL, new FindReplaceOptions(FindReplaceDirection.Forward));
+
+                doc.Range.Replace("AddresMailIndex", AddresMailIndex, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("AddresArea", AddresArea, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("AddresCity", AddresCity, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("AddresStreet", AddresStreet, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("AddresHome", AddresHome, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("ReportArea", ReportArea, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("ReportCity", ReportCity, new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("GUSZN", GUSZN, new FindReplaceOptions(FindReplaceDirection.Forward));
+
+
+                doc.Range.Replace("DateWorkerHomeTimeBegin", DateWorkerHomeTimeBegin.ToString("dd.mm.yyyy"), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("DateWorkerHomeTimeEnd", DateWorkerHomeTimeEnd.ToString("dd.mm.yyyy"), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WorkerHomeTimeI", WorkerHomeTimeI.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WorkerHomeTime", WorkerHomeTime.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WorkerHomeI", WorkerHomeI.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WorkerHome", WorkerHome.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("WorkerI", WorkerI.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                doc.Range.Replace("Worker", Worker.ToString(), new FindReplaceOptions(FindReplaceDirection.Forward));
+                
+                doc.Save(dialog.FileName, SaveFormat.Docx);
+                
+            }
         }
     }
 }
