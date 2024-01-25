370f4274a5bf305f294ed23a5377119d3ea3b613
mina987 <erm.555.mmm@gmail.com>
2024-01-25 10:04:44 +0300

    Add lib

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,17 @@
+using Aspose.Words;
+using ExamsCDSP.Properties;
+using System.Collections;
+using System.Reflection;
+
 namespace ExamsCDSP
 {
     public partial class Form1 : Form
     {
+        Document doc;
         public Form1()
         {
+            MemoryStream stream = new MemoryStream(Resources.report);
+            doc = new Document(stream);
             InitializeComponent();
         }
     }
